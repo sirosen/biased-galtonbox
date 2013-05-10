1458732d7f3469b29da79c88e9fc22ffc0d7c2c6
Stephen Rosen <sirosen@uchicago.edu>
2013-05-10 15:59:06 -0500

    Added methods to convert to a probability triangle and subtract a set of paths from the triangle.

--- triangle.py
+++ triangle.py
@@ -20,3 +20,26 @@ def pascal(depth):
             tri.append(new_row)
 
         return tri
+
+def probability_triangle(triangle):
+    new_triangle = []
+    for row in triangle:
+        s = sum(row)
+        new_row = [x/s for x in row]
+        new_triangle.append(new_row)
+    return new_triangle
+
+def subtract_path(triangle, row_n, col_n):
+    depth = len(triangle)
+    if row_n > depth:
+        raise Exception('Woah! We can\'t take things out of the triangle that are deeper than it!')
+
+    to_subtract = probability_triangle(pascal(depth - row_n))
+    v = triangle[row_n][col_n]
+    for sub_row in to_subtract:
+        for i in range(len(sub_row)):
+            sub_row[i] *= v
+
+    for r,sub_row in enumerate(to_subtract):
+        for c,val in enumerate(sub_row):
+            triangle[row_n+r][col_n+c] -= val
