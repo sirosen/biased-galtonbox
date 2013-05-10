c0593a6b0b9d26a1b2b38b37e9a326c3a6b471d4
Stephen Rosen <sirosen@uchicago.edu>
2013-05-10 17:48:02 -0500

    Added fixed method to calculate the distribution of probability mass over buckets for chi^2 with k=3

---
+++ calc_buckets.py
@@ -0,0 +1,19 @@
+from scipy.stats import chi2
+
+buckets = 30
+
+rv = chi2(3)
+cdf = rv.cdf
+top = 10.0
+
+bucket_size = top/buckets
+epsilon = bucket_size/10
+x_caps = []
+cur = bucket_size
+while cur < top + epsilon:
+    x_caps.append(cur)
+    cur = cur+bucket_size
+
+vals = [cdf(x) - cdf(x-bucket_size) for x in x_caps]
+
+print(vals)
